1c91177eaa12ee9a80051409fd98a5132e3c6171
Wendy You <wyou130@gmail.com>
2022-11-28 17:28:57 -0500

    Reversed string with for loop and built in array methods

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,6 +1,20 @@
 function reverseString(str) {
   // type your code here
+// With built in methods
+  // let array = str.split('')
+  // let reversedArray = array.reverse()
+  // let reversedString = reversedArray.join('')
+  // console.log(reversedString)
+// Without built in methods
+  let reverse = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i]
+  }
+  return reverse
+  // console.log(reverse)
 }
+
+// reverseString("wow this is cool")
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +31,9 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Initialize empty string
+// Take the last character
+// Add the second to last character 
+// Take the string so far
+// Add the next character to the current string 
