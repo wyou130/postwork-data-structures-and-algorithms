c70e20be600e57aee9728b276d0c2a960788f886
Wendy You <wyou130@gmail.com>
2022-12-01 16:48:18 -0500

    Logged numbers 0-9

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,13 @@
 function recursiveCount(num = 0) {
   // type your code here
+  // function only runs under the condition that num < 10
+  if (num < 10) {
+    // we want to print num, then add 1 to it
+    console.log(num)
+    num++
+    // we call the function itself so it can continue to run while the condition is true
+    recursiveCount(num)
+  }
 }
 
 if (require.main === module) {
