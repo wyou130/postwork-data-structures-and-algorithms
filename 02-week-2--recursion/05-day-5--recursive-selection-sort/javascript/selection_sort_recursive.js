0d5804438a67971808add460d238340e768f9b72
Wendy You <wyou130@gmail.com>
2022-12-07 15:29:53 -0500

    Sorted array by finding the minimum element, removing it from the original array, calling the function again on this shorter original array, then adding the min value to a new array and returning that

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,21 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  // recursion should end when all elements of the array are removed 
+  if (arr.length === 0) return []
+  // find minimum element in the array, using spread operator to access the elements as objects
+  let min = Math.min(...arr)
+  // find the index of this minimum element
+  let index = arr.indexOf(min)
+  // remove the minimum element from the array
+  arr.splice(index, 1)
+  // define the new array to be the result of this function
+  let newArray = selectionSortRecursive(arr)
+  // add the minimum element to the new array
+  // using unshift because recursion will completes last calls first, then work its way to the first calls
+  // push gives the new array in decreasing order
+  newArray.unshift(min)
+  // we want the new array returned with all elements sorted 
+  return newArray
 }
 
 if (require.main === module) {
@@ -8,9 +24,22 @@ if (require.main === module) {
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
   console.log("");
+
+  console.log("Expecting: [-10, 0, 2, 3, 8, 8]");
+  console.log("=>", selectionSortRecursive([8, 2, 8, -10, 0, 3]));
 }
 
 module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// return the sorted array when arr[i] <= arr[i + 1] for the entire array
+// compare two numbers and splice the array based on which number is greater 
+// let removed
+// arr[0] <= arr[1] ? arr : removed = arr.splice(0, 1)
+// arr.splice(1, 0, removed[0])
+
+// instead of the above, which is more non-recursive, 
+// can use Math.min to find the minimum value in the array
+// add minimum values to a new array and return the new array
