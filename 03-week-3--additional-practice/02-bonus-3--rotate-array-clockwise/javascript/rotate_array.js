7b276ca0f37bd8ce9c5748c43e29be3a69e585ea
Wendy You <wyou130@gmail.com>
2022-12-23 16:18:19 -0500

    Rotated array recursively using pop and unshift

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,5 +1,12 @@
 function rotateArray(arr, k) {
   // type your code here
+  if (k === 0 || arr.length === 0) return arr
+  const lastElement = arr.pop()
+  // console.log(lastElement)
+  arr.unshift(lastElement)
+  // console.log(arr)
+  return rotateArray(arr, k - 1)
+  // return arr
 }
 
 if (require.main === module) {
@@ -16,9 +23,27 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [10, 3, 'banana', 'x', 33, 5]");
+  console.log("=>", rotateArray([5, 10, 3, 'banana', 'x', 33], 5));
+
+  console.log("");
+
+  console.log("Expecting: ['x', 33, 5, 10, 3, 'banana']");
+  console.log("=>", rotateArray([5, 10, 3, 'banana', 'x', 33], 8));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", rotateArray([], 3));
 }
 
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// take the last element, remove it then add it to the beginning of the array
+// do this k times 
